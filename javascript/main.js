854edbad5ac64c8655c1fca116e747c49e5480f8
Dominik Aumayr <dominik@aumayr.name>
2016-02-21 21:07:42 +0100

    Add "Run in bean-query via fava"-button

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+var baseURL = "http://fava.pythonanywhere.com/query/?bql=";
+
+var forEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]); // passes back stuff we need
+  }
+};
+
+
+var codeBlocks = document.getElementsByClassName("highlight");
+
+forEach(codeBlocks, function (index, codeBlock) {
+    console.log(index, encodeURI(codeBlock.textContent)); // passes index + value back!
+    var createA = document.createElement('a');
+    var createAText = document.createTextNode("Run in bean-query in fava");
+    createA.setAttribute('href', baseURL + encodeURI(codeBlock.textContent));
+    createA.appendChild(createAText);
+    codeBlock.appendChild(createA);
+});
